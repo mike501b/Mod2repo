ede3dc07050d423019cec3b4495a3768a9a9b698
Mike <michael.blanchette3@gmail.com>
2022-12-14 10:14:46 -0500

    Added user prompt to save csv and moved to utils folder

--- loan_qualifier_app/qualifier/utils/fileio.py
+++ loan_qualifier_app/qualifier/utils/fileio.py
@@ -5,7 +5,8 @@ This contains a helper function for loading and saving CSV files.
 
 """
 import csv
-
+import sys
+import questionary
 
 def load_csv(csvpath):
     """Reads the CSV file from path provided.
@@ -28,3 +29,27 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+
+def save_qualifying_loans(qualifying_loans):
+    """Saves the qualifying loans to a CSV file.
+
+    Args:
+        qualifying_loans (list of lists): The qualifying bank loans.
+    """
+    # @TODO: Complete the usability dialog for savings the CSV Files.
+    # YOUR CODE HERE!
+    save_prompt = questionary.confirm("Would you like to save your loans to a csv file?", default=True).ask()
+    if save_prompt == True:
+        if len(qualifying_loans) == 0:
+            sys.exit("Sorry! There are no loans to save")
+        else:
+            header = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
+            csvpath = questionary.text("Enter a file path to save the file (.csv):").ask()
+            with open(csvpath, 'w', newline = '') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerow(header)
+                for row in qualifying_loans:
+                    csvwriter.writerow(row)
+    else:
+        print("You choose not to save a csv file")
